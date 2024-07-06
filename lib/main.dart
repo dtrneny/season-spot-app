77a2b64d8494c2a627c8e12d60ada201602fd015
dtrneny <dalibor.trneny00@gmail.com>
2024-07-06 20:53:52 +0200

    feat: added firebase configuration and firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:season_spot/core/themes/dark_theme.dart';
 import 'package:season_spot/core/themes/light_theme.dart';
 import 'package:season_spot/testing_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MainApp());
 }
 
--- lib/testing_page.dart
+++ lib/testing_page.dart
@@ -1,9 +1,23 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:season_spot/core/utils/localization.dart';
 
 class TestingPage extends StatelessWidget {
+  void testingFunction() async {
+    print("runned");
+    CollectionReference ref = FirebaseFirestore.instance.collection("client");
+    try {
+      DocumentReference result = await ref.add({
+        'name': "John Doe"
+      });
+      print("Document added with ID: ${result.id}");
+    } catch (e) {
+      print("Error adding document: $e");
+    }
+  }
+
   const TestingPage({super.key});
 
   @override
@@ -15,11 +29,13 @@ class TestingPage extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              const Text("Q6", style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+              const Text("Font test Q6", style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
               Text(t(context).helloWorld),
               ElevatedButton(
-                onPressed: () {},
-                child: const Text("Test 6"),
+                onPressed: () {
+                  testingFunction();
+                },
+                child: const Text("FIREBASE test"),
               )
             ],
           ),
